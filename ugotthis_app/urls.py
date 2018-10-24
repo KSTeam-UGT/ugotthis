b968592c7e92d8ac7736482823c670a42c0215ba
Steve Musial <sfmusial@hotmail.com>
2018-10-24 16:54:01 -0700

    Theme user page

--- ugotthis_app/urls.py
+++ ugotthis_app/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 
 from . import views
@@ -7,7 +7,19 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     # path('users/<username>/', views.user_page, name="user_page"),
+    path('users/<username>/', views.user_page, name="user_page"),
     path('register/', views.registration, name="registration"),
     path('logout/', views.logout_user, name="logout_user"),
-    # path('profile/', views.profile_page),
+    # path('profile/', views.profile_page, name="profile_page"),
+    # path('user-content/', views.user_content, name="user_content"),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
--- ugotthis_app/views.py
+++ ugotthis_app/views.py
@@ -6,6 +6,8 @@ from django.contrib import auth
 from django.contrib.auth import logout
 from django.contrib import messages
 
+import requests
+
 from .models import UserSetting
 
 
@@ -23,7 +25,8 @@ class NewUserForm(forms.Form):
         ('4', 'Peace, Meditation, Zen, Gratitude, Love'),
     )
     setting = forms.ChoiceField(widget=forms.RadioSelect,
-                                choices=choices)
+                                choices=choices,
+                                label='Choose your interests:',)
 
 
 class LoginForm(forms.Form):
@@ -41,14 +44,18 @@ class LoginForm(forms.Form):
 
 def logout_user(request):
     logout(request)
+    messages.success(request, 'You’ve been logged out.')
     return redirect('/')
 
 
 def homepage(request):
+    if request.user.is_authenticated:
+        return redirect('/users/' + str(request.user))
+
     if request.POST.get('submit') == 'login':
 
         # Create a form instance and populate it with data from the request
-        login_form = LoginForm(request.POST, prefix='login-form')
+        login_form = LoginForm(request.POST)
 
         if login_form.is_valid():
             username = request.POST.get('username', '')
@@ -57,51 +64,73 @@ def homepage(request):
 
             if user is not None:
                 auth.login(request, user)
+                return redirect('/users/' + username)
+            else:
+                messages.warning(request, "Incorrect username or password")
                 return redirect('/')
 
     else:
         # if a GET we'll create a blank form
-        login_form = LoginForm(prefix='login-form')
+        login_form = LoginForm()
 
     if request.POST.get('submit') == 'register':
 
         # Create a form instance and populate it with data from the request
-        registration_form = NewUserForm(request.POST, prefix='registration-form')
+        registration_form = NewUserForm(request.POST,
+                                        prefix='registration-form')
 
         if registration_form.is_valid():
-            # Create a new user object populated with the data we are
-            # giving it from the cleaned_data form
-            user = User.objects.create_user(
-                username=registration_form.cleaned_data['username'],
-                password=registration_form.cleaned_data['password'],
-                email=registration_form.cleaned_data['email'],
-            )
 
-            if registration_form.cleaned_data['setting'] == '1':
-                keywords = [1, 2, 3, 4, 5]
-            elif registration_form.cleaned_data['setting'] == '2':
-                keywords = [6, 7, 8]
-            elif registration_form.cleaned_data['setting'] == '3':
-                keywords = [9, 10, 11]
-            else:
-                keywords = [12, 13, 14, 15, 16]
-
-            # Get default keywords from db, based on user selection
-            for kw_id in keywords:
-                UserSetting.objects.create(
-                    keywords=UserSetting.objects.get(id=kw_id),
-                    user_id=user.id,
+            if not User.objects.filter(
+                username=registration_form.cleaned_data['username']
+            ).exists() and not User.objects.filter(
+                email=registration_form.cleaned_data['email']
+            ).exists():
+                # Create a new user object populated with the data we are
+                # giving it from the cleaned_data form
+                user = User.objects.create_user(
+                    username=registration_form.cleaned_data['username'],
+                    password=registration_form.cleaned_data['password'],
+                    email=registration_form.cleaned_data['email'],
                 )
 
-            # As soon as our new user is created, we make this user be
-            # instantly "logged in".
-            auth.login(request, user)
-            return redirect('/')
+                if registration_form.cleaned_data['setting'] == '1':
+                    keywords = [1, 2, 3, 4, 5]
+                elif registration_form.cleaned_data['setting'] == '2':
+                    keywords = [6, 7, 8]
+                elif registration_form.cleaned_data['setting'] == '3':
+                    keywords = [9, 10, 11]
+                else:
+                    keywords = [12, 13, 14, 15, 16]
+
+                # Get default keywords from db, based on user selection
+                for kw_id in keywords:
+                    UserSetting.objects.create(
+                        keywords=UserSetting.objects.get(id=kw_id),
+                        user_id=user.id,
+                    )
+
+                auth.login(request, user)
+                # As soon as our new user is created, we make this user be
+                # instantly "logged in".
+                messages.success(request, 'Registration successful. '
+                                            'Welcome to U Got This!')
+                return redirect('/users/' + str(user))
+
+            elif User.objects.filter(
+                username=registration_form.cleaned_data['username']
+            ).exists() or User.objects.filter(
+                email=registration_form.cleaned_data['email']
+            ).exists():
+                print('USER OR EMAIL EXISTS')
+                messages.warning(request, 'Account already exists.')
+                return redirect('/')
 
     else:
         # if a GET we'll create a blank form
         registration_form = NewUserForm(prefix='registration-form')
 
+    # import IPython; IPython.embed()
     context = {
         'login_form': login_form,
         'registration_form': registration_form,
@@ -153,3 +182,88 @@ def registration(request):
         'form': form,
     }
     return render(request, 'pages/registration.html', context)
+
+
+def user_page(request, username):
+    if not request.user.is_authenticated:
+        print('TEST')
+        messages.warning(request, "Please log in to view that page")
+        return redirect('/')
+
+    if not User.objects.filter(username=username).exists() and (
+        request.user.is_authenticated
+    ):
+        return redirect('/users/' + request.user.username)
+
+    user = User.objects.get(username=username)
+    keywords = UserSetting.objects.order_by('-created')
+    keywords_by_user = keywords.filter(user=user)
+
+    books = []
+    videos = []
+    for word in keywords_by_user:
+        search_string = str(word).lower()
+
+        # Get books from OpenLibrary
+        books_response_subject = requests.get(
+            f'https://openlibrary.org/subjects/{word}.json'
+        )
+        data_subject = books_response_subject.json()
+
+        books_by_subject = data_subject['works']
+        books_by_subject_results = []
+        for work in books_by_subject:
+            for book in work['subject']:
+                if search_string in str(book).lower():
+                    books_by_subject_results.append(work['title'])
+        books_by_subject_results = list(set(books_by_subject_results))
+
+        for work in books_by_subject_results:
+            for book in books_by_subject:
+                if work == book['title']:
+                    authors = []
+                    for author in book['authors']:
+                        authors.append(author['name'])
+                    books.append(
+                        {
+                            'title': book['title'],
+                            'cover_id': book['cover_id'],
+                            'link_key': book['key'],
+                            'authors': authors,
+                        },
+                    )
+
+        # Get videos from YouTube
+        response_video = requests.get(
+            'https://www.googleapis.com/youtube/v3/search?'
+            'part=snippet&maxResults=50&q=' + search_string +
+            '&key=AIzaSyBXLC_j264f9ZUllnvEidYIBAVckJVI5cI&maxResults=1'
+            '&safeSearch=strict&type=video'
+        )
+        data_video = response_video.json()
+        videos_by_search_string = data_video['items']
+        for video in videos_by_search_string:
+            if 'videoId' in video['id']:
+                videos.append(
+                    {
+                        'id': video['id']['videoId'],
+                        'title': video['snippet']['title'],
+                        'thumbnail': video['snippet']['thumbnails']['medium']['url'],
+                        'link': 'https://www.youtube.com/watch?v=' + video['id']['videoId'],
+                        'description': video['snippet']['description'],
+                        
+                    }
+                )
+                # print('https://www.youtube.com/watch?v=' + video['id']['videoId'])
+        # videos = list(set(videos))
+
+
+
+    books = sorted(books, key=lambda k: k['title'])
+
+    context = {
+        'keywords': keywords_by_user,
+        'books': books,
+        'videos': videos,
+    }
+    return render(request, 'pages/user_content.html', context)
