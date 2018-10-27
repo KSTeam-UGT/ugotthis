d14c3f98a80511a97d8c0760cf00f7e6cc382d5b
Steve Musial <sfmusial@hotmail.com>
2018-10-26 20:09:11 -0700

    More static tweaks

--- ugotthis/settings.py
+++ ugotthis/settings.py
@@ -137,11 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'ugotthis_app/static')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+print(STATIC_ROOT)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
